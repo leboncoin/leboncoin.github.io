96d71a354a52af1115b01eaed4b26e0569038873
Andy Ladjadj <andy.ladjadj@schibsted.com>
2018-10-26 14:37:22 +0200

    add html stylesheet

--- src/components/layout.js
+++ src/components/layout.js
@@ -28,7 +28,7 @@ const Layout = ({ children }) => (
           ]}
         >
           <html lang="en" />
-        </Helmet>
+      </Helmet>
         <header id={"home"}>
           <div className="bg-img" style={{backgroundImage: "url('../static/background1.jpg')"}}>
             <div className="overlay"/>
@@ -38,7 +38,9 @@ const Layout = ({ children }) => (
         </header>
         <div>
           {children}
-
+          <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700%7CVarela+Round" rel="stylesheet"/>
+          <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous"/>
+          <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous"/>
           {/* jQuery Plugins*/}
           <script type="text/javascript" src="js/jquery.min.js"></script>
           <script type="text/javascript" src="js/bootstrap.min.js"></script>
