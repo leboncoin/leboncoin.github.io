e65e6562b37755eeedf98a2b99e5fbec14d027fb
Paul Lhussiez <paul.lhussiez@schibsted.com>
2018-10-29 13:51:41 +0100

    Change URL in project, also delete a bunch of useless CSS

--- src/components/project.js
+++ src/components/project.js
@@ -6,9 +6,9 @@ export class ProjectTile extends React.Component {
   render() {
     return (
       <div className="col-sm-4 col-md-4">
-        <a className="project" style={{display: "block"}} href="https://google.com">
+        <a className="project" target="_blank" rel="noopener noreferrer" href={this.props.project.url}>
             <FontAwesomeIcon icon={faGithub} />{" "}
-            <h3><a target="_blank" rel="noopener noreferrer" href={this.props.project.url}>{this.props.project.name}</a></h3>
+            <h3>{this.props.project.name}</h3>
             <p>{this.props.project.description}</p>
             {this.props.project.topics.map((topic) => <span>{topic}</span>)}
         </a>
