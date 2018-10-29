dd6cb4a796af1394fb980c3182907237257db7bb
Paul Lhussiez <paul.lhussiez@schibsted.com>
2018-10-29 10:32:59 +0100

    Fixing project height

--- src/components/sections/projects.js
+++ src/components/sections/projects.js
@@ -1,45 +1,40 @@
 import Section from "./layout";
 import React from "react";
-import {graphql, StaticQuery} from "gatsby";
+import { graphql, StaticQuery } from "gatsby";
 import Project from "../project";
 
 const GithubReposQuery = graphql`
-query {
-  allGithubRepos(sort: { fields: [stars], order: DESC } ){
-    edges {
-    	node {
-        id
-        name
-        url
-        stars
-        description
+  query {
+    allGithubRepos(sort: { fields: [stars], order: DESC }) {
+      edges {
+        node {
+          id
+          name
+          url
+          stars
+          description
+        }
       }
     }
   }
-}`;
+`;
 
 export default () => {
   return (
     <StaticQuery
       query={GithubReposQuery}
-      render={
-        data => {
-          const {edges: projects} = data.allGithubRepos;
-          return (
-            <Section
-              sectionName={"projects"}
-              sectionTitle={"Projects"}
-            >
-
-              <div className="row" style={{display: "flex", flexWrap: "wrap"}}>
-                {projects.map(({ node }, index) =>
-                  <Project key={index} project={node} />
-                )}
-              </div>
-            </Section>
-          )
-        }
-      }
+      render={data => {
+        const { edges: projects } = data.allGithubRepos;
+        return (
+          <Section sectionName={"projects"} sectionTitle={"Projects"}>
+            <div className="row" style={{ display: "flex", flexWrap: "wrap" }}>
+              {projects.map(({ node }, index) => (
+                <Project key={index} project={node} />
+              ))}
+            </div>
+          </Section>
+        );
+      }}
     />
-  )
-}
+  );
+};
