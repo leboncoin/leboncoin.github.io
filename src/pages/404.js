841d754835c14799a138f78b4d5252607a589246
andy ladjadj <andy.ladjadj@gmail.com>
2019-03-09 17:48:11 +0100

    improve 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import Layout from '../components/layout'
-
+import Section from '../components/sections/layout'
 const NotFoundPage = () => (
   <Layout>
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <Section sectionName={"404"} sectionTitle={"Oops"}>
+          <p style={{textAlign: "center"}} >Tu t'es égaré</p>
+    </Section>
   </Layout>
 )
 
