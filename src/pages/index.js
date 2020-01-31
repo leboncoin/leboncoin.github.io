e55889ed2b80f2a05a68de6c32295815c7b21ce8
Paul Lhussiez <paul.lhussiez@adevinta.com>
2020-01-31 18:18:22 +0100

    Cleanup

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,18 @@
-import React from 'react'
-import Layout from '../components/layout'
-import Blog from "../components/sections/blog"
-import Projects from "../components/sections/projects"
-import {Keys, SecondKeys} from "../components/sections/keys"
-import Youtube from "../components/sections/youtube"
+import React from "react";
+import Layout from "../components/layout";
+import Blog from "../components/sections/blog";
+import Projects from "../components/sections/projects";
+import { Keys, SecondKeys } from "../components/sections/keys";
+import Youtube from "../components/sections/youtube";
 
 const IndexPage = () => (
   <Layout>
-    <Blog/>
-    <Keys/>
-    <Youtube/>
-    <SecondKeys/>
-    <Projects/>
+    <Blog />
+    <Keys />
+    <Youtube />
+    <SecondKeys />
+    <Projects />
   </Layout>
 );
 
-export default IndexPage
+export default IndexPage;
